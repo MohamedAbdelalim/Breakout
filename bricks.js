734b40e190d843113eb42776ef2e56ff1676adc6
Mohab-Gera <mohabmohamedgera@gmail.com>
2025-09-19 22:28:37 +0300

    draw top box and score box

--- bricks.js
+++ bricks.js
@@ -60,3 +60,48 @@ function drawBricks() {
         }
     }
 }
+
+
+
+let score =0;
+
+// topBox for declare the the score, high score , and lives.
+
+let topBox = {
+    x: 10,
+    y: 10,
+    width: canvasWidth - 20,
+    height: 80
+};
+
+function drawTopBox() {
+  ctx.strokeStyle = "white";  
+  ctx.lineWidth = 3;        
+  ctx.strokeRect(topBox.x, topBox.y,topBox.width, topBox.height); 
+};
+
+drawTopBox()
+
+// draw the score box and its text 
+
+let scoreBox = {
+      x: topBox.width * .75,
+      y: topBox.y * 1.4,
+      width: canvasWidth * .25,
+      height: topBox.height - 8
+}
+
+function drawScoreBox(score) {
+
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 3;
+    ctx.strokeRect(scoreBox.x, scoreBox.y, scoreBox.width, scoreBox.height);
+
+    ctx.fillStyle = "white"
+    ctx.font = "30px Arial"
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle"; 
+    ctx.fillText(`Score: ${score}`, scoreBox.x + scoreBox.width / 2, scoreBox.y + scoreBox.height / 2);
+   };
+
+drawScoreBox(score)
